70df2b1189c61e92808e8962fd1031b6a6e1626c
FrogO2 <1302207844@qq.com>
2026-01-31 18:00:03 +0800

    combine level

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -7,7 +7,14 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UIMgr.Instance.ShowPanel<LoginPanel>();
+        UIMgr.Instance.ShowPanel<LoginPanel>(E_UILayer.Middle, (BasePanel) =>
+        {
+            ResMgr.Instance.LoadAsync<GameObject>("ResourcesMgr/ResourcesMgr", (obj) =>
+            {
+                Instantiate(obj);
+            });;
+        });
+        
     }
 
     // Update is called once per frame

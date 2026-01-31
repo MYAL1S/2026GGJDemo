e29d6a23ef152b05d2ff61e2a35b49847e1e9d2c
FrogO2 <1302207844@qq.com>
2026-01-31 11:28:17 +0800

    minimap update

--- Assets/Scripts/UI/CamFollow.cs
+++ Assets/Scripts/UI/CamFollow.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CamFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Camera assignedCamera; // Assign the camera in the Inspector
+    private Camera mainCamera;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        FollowMouse();
+    }
+
+    private void Start()
+    {
+        assignedCamera = GetComponent<Camera>();
+        mainCamera = Camera.main;
+    }
+
+    private void FollowMouse()
+    {
+        if (assignedCamera == null)
+        {
+            Debug.LogWarning("Assigned camera is not set.");
+            return;
+        }
+
+        // Get the mouse position in world space
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+            Input.mousePosition.y, assignedCamera.nearClipPlane));
+
+        // Smoothly move the camera towards the mouse position
+        Vector3 targetPosition = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+        transform.position = targetPosition;
     }
 }

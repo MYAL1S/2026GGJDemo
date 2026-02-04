00a0a9b973eab3e4a293c7ce32f4ef1bd4757007
zc <1849197339@qq.com>
2026-02-04 16:52:16 +0800

    Change The Method Of IntroPanel

--- Assets/Scripts/UI/Panel/IntroPanel.cs
+++ Assets/Scripts/UI/Panel/IntroPanel.cs
@@ -8,6 +8,7 @@ public class IntroPanel : BasePanel
     private Image imgIntro1;
     private Image imgIntro2;
     private Image imgIntro3;
+    private Image nowIntro;
 
     public override void Init()
     {
@@ -17,48 +18,12 @@ public class IntroPanel : BasePanel
         imgIntro3 = GetControl<Image>("ImgIntro3");
         imgIntro2.gameObject.SetActive(false);
         imgIntro3.gameObject.SetActive(false);
+        nowIntro = imgIntro1;
     }
 
     public override void ShowMe()
     {
         base.ShowMe();
-
-        TimerMgr.Instance.CreateTimer(false, 5000, () => 
-        {
-            imgIntro2.gameObject.SetActive(true);
-            TimerMgr.Instance.CreateTimer(false, 5000, () => 
-            {
-                imgIntro3.gameObject.SetActive(true);
-                TimerMgr.Instance.CreateTimer(false, 5000, () => 
-                {
-                    // 重置所有游戏数据
-                    ResetAllGameData();
-
-                    // ⭐ 清理音效列表
-                    MusicMgr.Instance.ClearSound();
-
-                    // 清理所有面板
-                    UIMgr.Instance.ClearAllPanels();
-
-                    SceneMgr.Instance.LoadSceneAsync("GameScene", () =>
-                    {
-                        // 显示游戏界面
-                        UIMgr.Instance.ShowPanel<UIBackgroundPanel>(E_UILayer.Bottom, (bgPanel) =>
-                        {
-                            UIMgr.Instance.ShowPanel<GamePanel>(E_UILayer.Middle, (gamePanel) =>
-                            {
-                                ElevatorMgr.Instance.StartElevator();
-                            });
-                        });
-                    });
-                });
-            });
-        });
-
-        //TimerMgr.Instance.CreateTimer(false, 100, () => 
-        //{
- 
-        //});
     }
 
     /// <summary>
@@ -79,5 +44,54 @@ public class IntroPanel : BasePanel
         EventMgr.Instance.ResetState();
         
         Debug.Log("[IntroPanel] 所有游戏数据已重置");
+    }
+
+    /// <summary>
+    /// 用于切换介绍图片的状态
+    /// </summary>
+    private void ChangeState()
+    {
+        switch (nowIntro.name)
+        {
+            case "ImgIntro1":
+                imgIntro2.gameObject.SetActive(true);
+                nowIntro = imgIntro2;
+                break;
+            case "ImgIntro2":
+                imgIntro3.gameObject.SetActive(true);
+                nowIntro = imgIntro3;
+                break;
+            case "ImgIntro3":
+                // 重置所有游戏数据
+                ResetAllGameData();
+
+                // ⭐ 清理音效列表
+                MusicMgr.Instance.ClearSound();
+
+                // 清理所有面板
+                UIMgr.Instance.ClearAllPanels();
+
+                SceneMgr.Instance.LoadSceneAsync("GameScene", () =>
+                {
+                    // 显示游戏界面
+                    UIMgr.Instance.ShowPanel<UIBackgroundPanel>(E_UILayer.Bottom, (bgPanel) =>
+                    {
+                        UIMgr.Instance.ShowPanel<GamePanel>(E_UILayer.Middle, (gamePanel) =>
+                        {
+                            ElevatorMgr.Instance.StartElevator();
+                        });
+                    });
+                });
+                break;
+        }
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeState();
+        }
     }
 }
